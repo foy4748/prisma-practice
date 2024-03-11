5fc62b37bd51accdbd4d13420c615f21d1991669
foy4748 <faisaljfcl@gmail.com>
2024-03-12 01:14:49 +0600

    Created a Book, then created authors for it

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 const prismaClient = new PrismaClient();
 
 async function main() {
+  // Creating Author, then its books
+  /*
   const response = await prismaClient.author.create({
     data: {
       name: "Author 1",
@@ -32,6 +34,40 @@ async function main() {
       },
     },
   });
+  */
+  // Creating Books, then its authors
+  /*
+  const response = await prismaClient.book.create({
+    data: {
+      name: "Book 1",
+      Author_Book: {
+        create: [
+          {
+            author: {
+              create: {
+                name: "Author 1",
+              },
+            },
+          },
+          {
+            author: {
+              create: {
+                name: "Author 2",
+              },
+            },
+          },
+          {
+            author: {
+              create: {
+                name: "Author 3",
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
+  */
 }
 
 main();
